7402bc7d2b12085622c6e1fb3f875b707aa785d0
Yoko Harada <yokolet@gmail.com>
2019-08-13 16:33:33 -0700

    chore: improves add binary
    The solution, add binary, in the arrays and strings section was updated
    to have better solution.

--- tranquil-beach/arrays_strings/add_binary.py
+++ tranquil-beach/arrays_strings/add_binary.py
@@ -1,5 +1,5 @@
 class AddBinary:
-    def addBinary(self, a, b):
+    def addBinary4(self, a, b):
         """
         :type a: str
         :type b: str
@@ -7,20 +7,14 @@ class AddBinary:
         """
         return bin(int(a, 2) + int(b, 2))[2:]
 
-    def addBinary2(self, a, b):
-        """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
+    def addBinary(self, a: str, b: str) -> str:
         result = ''
         idx_a, idx_b = len(a) - 1, len(b) - 1
         carry = 0
         while idx_a >= 0 or idx_b >= 0:
-            d_a = 1 if idx_a >= 0 and a[idx_a] == '1' else 0
-            d_b = 1 if idx_b >= 0 and b[idx_b] == '1' else 0
-            tmp = carry + d_a + d_b
-            carry, rem = tmp // 2, tmp % 2
+            v_a = 1 if idx_a >= 0 and a[idx_a] == '1' else 0
+            v_b = 1 if idx_b >= 0 and b[idx_b] == '1' else 0
+            carry, rem = divmod(carry + v_a + v_b, 2)
             result = str(rem) + result
             idx_a -= 1
             idx_b -= 1
@@ -45,3 +39,33 @@ class AddBinary:
         if add==1:
             return "1"+s
         return s
+
+    def addBinary2(self, a: 'str', b: 'str') -> 'str':
+        table = {
+            (False, False, False): (False, '0'), # carry and value
+            (False, False, True): (False, '1'),
+            (False, True, False): (False, '1'),
+            (False, True, True): (True, '0'),
+            (True, False, False): (False, '1'),
+            (True, False, True): (True, '0'),
+            (True, True, False): (True, '0'),
+            (True, True, True): (True, '1'),
+            (False, False): (False, '0'),
+            (False, True): (False, '1'),
+            (True, False): (False, '1'),
+            (True, True): (True, '0')
+        }
+        idx_a, idx_b = len(a)-1, len(b)-1
+        carry, result = False, ''
+        while idx_a >= 0 and idx_b >= 0:
+            carry, c = table[(carry, a[idx_a]=='1', b[idx_b]=='1')]
+            result = c + result
+            idx_a -= 1
+            idx_b -= 1
+        (idx, s) = (idx_a, a) if idx_a >= 0 else (idx_b, b)
+        while carry and idx >=0:
+            carry, c = table[(carry, s[idx]=='1')]
+            result = result + c
+            idx -= 1
+        if carry: result = '1' + result
+        return result
