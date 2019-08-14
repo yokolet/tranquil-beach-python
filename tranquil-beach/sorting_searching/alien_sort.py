564b3fca264d058967c23bca65cbd603ad4864c9
Yoko Harada <yokolet@gmail.com>
2019-08-13 19:52:39 -0700

    feat: adds new solutions to alien sort
    Two new solutions were added to alien sort, which is in the sorting and
    seaching section.

--- tranquil-beach/sorting_searching/alien_sort.py
+++ tranquil-beach/sorting_searching/alien_sort.py
@@ -1,5 +1,21 @@
 class AlienSort:
     def isAlienSorted(self, words: 'List[str]', order: str) -> bool:
+        memo = {c: i for i, c in enumerate(order)}
+        prev = words[0]
+        for cur in words[1:]:
+            i, j = 0, 0
+            while i < len(prev) and j < len(cur) and prev[i] == cur[j]:
+                i += 1
+                j += 1
+            if i < len(prev) and j < len(cur) and memo[prev[i]] > memo[cur[j]]:
+                return False
+            if len(prev) > len(cur) and prev[i-1] == cur[j-1]:
+                return False
+            prev = cur
+        return True
+
+
+    def isAlienSorted2(self, words: 'List[str]', order: str) -> bool:
         memo = {}
         for i, c in enumerate(order):
             memo[c] = i
@@ -18,3 +34,8 @@ class AlienSort:
                 return False
             prev = cur
         return True
+
+    def isAlienSorted3(self, words: 'List[str]', order: str) -> bool:
+        order_map={c:i for i,c in enumerate(order)}  # create a hashmap
+        wordIndices=[[order_map[c] for c in word] for word in words]
+        return all(w1<=w2 for w1,w2 in zip(wordIndices,wordIndices[1:]))
--- tranquil-beach/test/sorting_searching_test/test_alien_sort.py
+++ tranquil-beach/test/sorting_searching_test/test_alien_sort.py
@@ -5,19 +5,23 @@ from sorting_searching.alien_sort import AlienSort
 
 class TestAlienSort(unittest.TestCase):
     def setUp(self):
-        self.func = AlienSort()
+        self.func = AlienSort().isAlienSorted
 
     def test_1(self):
         words, order = ["hello","leetcode"], "hlabcdefgijkmnopqrstuvwxyz"
-        self.assertTrue(self.func.isAlienSorted(words, order))
+        self.assertTrue(self.func(words, order))
 
     def test_2(self):
         words, order = ["word","world","row"], "worldabcefghijkmnpqstuvxyz"
-        self.assertFalse(self.func.isAlienSorted(words, order))
+        self.assertFalse(self.func(words, order))
 
     def test_3(self):
         words, order = ["apple","app"], "abcdefghijklmnopqrstuvwxyz"
-        self.assertFalse(self.func.isAlienSorted(words, order))
+        self.assertFalse(self.func(words, order))
+
+    def test_4(self):
+        words, order = ["kuvp","q"], "ngxlkthsjuoqcpavbfdermiywz"
+        self.assertTrue(self.func(words, order))
 
 if __name__ == '__main__':
     unittest.main()
