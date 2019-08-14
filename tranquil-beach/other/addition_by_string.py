742c2a79c75fe361ed008f6e5bb1c37e1d01a505
Yoko Harada <yokolet@gmail.com>
2019-08-13 20:23:01 -0700

    chore: updates addition by string
    An existing solution, addition by string, in other section was updated.

--- tranquil-beach/other/addition_by_string.py
+++ tranquil-beach/other/addition_by_string.py
@@ -2,11 +2,9 @@ class AdditionByString:
     def addStrings(self, num1: str, num2: str) -> str:
         i, j, carry, result = len(num1) - 1, len(num2) - 1, 0, ""
         while i >= 0 or j >= 0 or carry > 0:
-            if i >= 0:
-                carry += int(num1[i])
-            if j >= 0:
-                carry += int(num2[j])
-            carry, v = divmod(carry, 10)
+            v_1 = int(num1[i]) if i >= 0 else 0
+            v_2 = int(num2[j]) if j >= 0 else 0
+            carry, v = divmod(carry + v_1 + v_2, 10)
             result = str(v) + result
             i -= 1
             j -= 1
