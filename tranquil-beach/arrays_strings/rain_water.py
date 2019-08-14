173c3adab01de2c7e0a9c769f3b54e2a35e0e78d
Yoko Harada <yokolet@gmail.com>
2019-08-13 19:09:35 -0700

    chore: updates rain water
    An existing solution, rain water, in the arrays and strings section was
    updated.

--- tranquil-beach/arrays_strings/rain_water.py
+++ tranquil-beach/arrays_strings/rain_water.py
@@ -1,21 +1,17 @@
 class RainWater:
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
+    def trap(self, height: 'List[int]') -> int:
         if not height or len(height) < 3: return 0
-    
         left, right, total = 0, len(height) - 1, 0
         left_max, right_max = height[left], height[right]
         while left < right:
-            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
             if left_max <= right_max:
                 total += left_max - height[left]
                 left += 1
+                left_max = max(left_max, height[left])
             else:
                 total += right_max - height[right]
                 right -= 1
+                right_max = max(right_max, height[right])
         return total
 
     def trap1(self, height):
