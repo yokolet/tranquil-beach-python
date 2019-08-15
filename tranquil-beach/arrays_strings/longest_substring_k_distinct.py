550cc642993f06d1d4e0d585fd55200f817af8b4
Yoko Harada <yokolet@gmail.com>
2019-08-14 21:49:05 -0700

    chore: updates longest substring k distinct
    An existing solution, longesg, substring with at most k distinct
    characters, in the arrays and strings section was updated.

--- tranquil-beach/arrays_strings/longest_substring_k_distinct.py
+++ tranquil-beach/arrays_strings/longest_substring_k_distinct.py
@@ -1,12 +1,11 @@
 class LongestSubstringKDistinct:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        left, max_length, memo = 0, 0, {}
+        left, max_len, memo = 0, 0, {}
         for i, char in enumerate(s):
             memo[char] = i
             if len(memo) > k:
                 left = min(memo.values())
                 del memo[s[left]]
                 left += 1
-            if i - left + 1 > max_length:
-                max_length = i - left + 1
-        return max_length
+            max_len = max(max_len, i-left+1)
+        return max_len
