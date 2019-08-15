643ab24042d591a66401c270f4bd5d6e13f89c9f
Yoko Harada <yokolet@gmail.com>
2019-08-15 10:17:48 -0700

    chore: updates max subarray sum k
    An existing solution, maximum size subarray sum equals k, in the arrays
    and strings section was updated.

--- tranquil-beach/arrays_strings/max_subarray_sum_k.py
+++ tranquil-beach/arrays_strings/max_subarray_sum_k.py
@@ -1,15 +1,10 @@
 class MaxSubarraySumK:
-    def maxSubArrayLen(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        max_length, sum_sofar, memo = 0, 0, {0: -1} # {sum: index}
+    def maxSubArrayLen(self, nums: 'List[int]', k: int) -> int:
+        max_len, acc, memo = 0, 0, {0: -1} # {sum: index}
         for i in range(len(nums)):
-            sum_sofar += nums[i]
-            if sum_sofar - k in memo:
-                max_length = max(max_length, i - memo[sum_sofar - k])
-            if sum_sofar not in memo:
-                memo[sum_sofar] = i
-        return max_length
+            acc += nums[i]
+            if acc - k in memo:
+                max_len = max(max_len, i - memo[acc - k])
+            if acc not in memo:
+                memo[acc] = i
+        return max_len
