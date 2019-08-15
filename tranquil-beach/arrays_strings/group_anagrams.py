1df9d45b69845e58dc71f61676628a92541fdace
Yoko Harada <yokolet@gmail.com>
2019-08-14 21:14:49 -0700

    chore: updates group anagrams
    An existing solution, group anagrams, in the arrays and strings section
    was updated.

--- tranquil-beach/arrays_strings/group_anagrams.py
+++ tranquil-beach/arrays_strings/group_anagrams.py
@@ -1,5 +1,16 @@
+from collections import defaultdict
+
 class GroupAnagrams:
     def groupAnagrams(self, strs: 'List[str]') -> 'List[List[str]]':
+        groups = defaultdict(list)
+        for s in strs:
+            h = 0
+            for c in s:
+                h += hash(c)
+            groups[h].append(s)
+        return [v for v in groups.values()]
+
+    def groupAnagrams2(self, strs: 'List[str]') -> 'List[List[str]]':
         groups = {}
         for s in strs:
             key = ''.join(sorted(s))
