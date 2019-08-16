4ba9d5783064665dc36ecc00c16837dc155bcd21
Yoko Harada <yokolet@gmail.com>
2019-08-15 22:41:37 -0700

    chore: updates stock
    An existing solugion, best time to buy and sell stock, in the dp section
    was updated.

--- tranquil-beach/dp/stock.py
+++ tranquil-beach/dp/stock.py
@@ -1,14 +1,9 @@
 class Stock:
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        min_price = float('inf')
-        profit = 0
-        for i in range(len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            elif prices[i] - min_price > profit:
-                profit = prices[i] - min_price
+    def maxProfit(self, prices: 'List[int]') -> int:
+        profit, min_price = 0, float('inf')
+        for p in prices:
+            if p < min_price:
+                min_price = p
+            elif p - min_price > profit:
+                profit = p - min_price
         return profit
