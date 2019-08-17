88fce442ff7f9f7c52927cb7558ae67797d6e4f1
Yoko Harada <yokolet@gmail.com>
2019-08-17 15:45:52 -0700

    chore: updates invalid parentheses
    An existing solution, remove invalid parentheses, in the backtracking
    section was updated.

--- tranquil-beach/backtracking/invalid_parentheses.py
+++ tranquil-beach/backtracking/invalid_parentheses.py
@@ -1,33 +1,29 @@
 class InvalidParentheses:
-    def removeInvalidParentheses(self, s):
-        """
-        :type s: str
-        :rtype: List[str]
-        """
-        def remove(s, last_i, last_j, parens, result):
-            i, count = last_i, 0
-            while i < len(s):
-                if s[i] == parens[0]:
+    def removeInvalidParentheses(self, s: str) -> 'List[str]':
+        def remove(s, left, right, parens):
+            count = 0
+            while right < len(s):
+                if s[right] == parens[0]:
                     count += 1
-                elif s[i] == parens[1]:
+                elif s[right] == parens[1]:
                     count -= 1
                 
                 if count >= 0:
-                    i += 1
+                    right += 1
                     continue
                 
-                j = last_j
-                while j <= i:
-                    if s[j] == parens[1] and (j == last_j or s[j - 1] != s[j]):
-                        remove(s[:j] + s[j+1:], i, j, parens, result)
-                    j += 1
+                start = left
+                while left <= right:
+                    if s[left] == parens[1] and (left == start or s[left - 1] != parens[1]):
+                        remove(s[:left] + s[left+1:], left, right, parens)
+                    left += 1
                 return
 
             if parens[1] == ')':
-                remove(s[::-1], 0, 0, (')', '('), result)
+                remove(s[::-1], 0, 0, (')', '('))
             else:
                 result.append(s[::-1])
 
         result = []
-        remove(s, 0, 0, ('(', ')'), result)
+        remove(s, 0, 0, ('(', ')'))
         return result
