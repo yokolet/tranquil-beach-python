4cf7f2c6239ee256f785701289f0ab9d2d5b80e9
Yoko Harada <yokolet@gmail.com>
2019-08-18 15:11:51 -0700

    chore: updates min meeting room
    An existing solution, minimum meeting rooms, in the sorting and
    searching section was updated.

--- tranquil-beach/test/sorting_searching_test/test_min_meeting_rooms.py
+++ tranquil-beach/test/sorting_searching_test/test_min_meeting_rooms.py
@@ -1,30 +1,26 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 import unittest
-from sorting_searching.interval import Interval
 from sorting_searching.min_meeting_rooms import MinRooms
 
 class TestMinRooms(unittest.TestCase):
     def setUp(self):
-        self.func = MinRooms()
+        self.func = MinRooms().minMeetingRooms
 
     def test_1(self):
-        arr = [[0, 30],[5, 10],[15, 20]]
+        intervals = [[0, 30],[5, 10],[15, 20]]
         expected = 2
-        intervals = [Interval(s, e) for [s, e] in arr]
-        self.assertEqual(self.func.minMeetingRooms(intervals), expected)
+        self.assertEqual(self.func(intervals), expected)
 
     def test_2(self):
-        arr =  [[7,10],[2,4]]
+        intervals =  [[7,10],[2,4]]
         expected = 1
-        intervals = [Interval(s, e) for [s, e] in arr]
-        self.assertEqual(self.func.minMeetingRooms(intervals), expected)
+        self.assertEqual(self.func(intervals), expected)
 
     def test_3(self):
-        arr = [[2,11],[6,16],[11,16]]
+        intervals = [[2,11],[6,16],[11,16]]
         expected = 2
-        intervals = [Interval(s, e) for [s, e] in arr]
-        self.assertEqual(self.func.minMeetingRooms(intervals), expected)
+        self.assertEqual(self.func(intervals), expected)
 
 if __name__ == '__main__':
     unittest.main()
