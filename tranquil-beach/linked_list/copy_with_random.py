9ee2084d039af9def1043f4b515ca2b84db142ee
Yoko Harada <yokolet@gmail.com>
2019-09-16 13:45:14 -0700

    feat: adds more solution to copy with random
    An exsiting solution, copy list wiht random pointer, got a better
    solution.

--- tranquil-beach/linked_list/copy_with_random.py
+++ tranquil-beach/linked_list/copy_with_random.py
@@ -1,22 +1,29 @@
 from .node import Node
+from collections import defaultdict
 
 class CopyWithRandom:
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        head2 = Node(None, None, None)
+    def copyRandomList(self, head: Node) -> Node:
+        cache = defaultdict(lambda: Node(None, None, None))
+        cache[None] = None  # to avoid if statement in the loop
         cur = head
-        cache = {} # (key, value) = (cur, ref), copied instance
-        prev = head2
         while cur:
-            n = Node(cur.val, None, None)
-            prev.next = n
-            cache[cur.val] = n
-            prev = n
+            cache[cur].val = cur.val
+            cache[cur].next = cache[cur.next]
+            cache[cur].random = cache[cur.random]
             cur = cur.next
-        cur = head
-        cur2 = head2.next
+        return cache[head]
+
+    def copyRandomList2(self, head: 'Node') -> 'Node':
+        dummy = Node(None, None, None)
+        cur, prev = head, dummy
+        cache = {} # (key, value) = (cur, ref), copied instance
+        while cur:
+            prev.next = Node(cur.val, None, None)
+            cache[cur.val] = prev.next
+            cur, prev = cur.next, prev.next
+        cur, copy = head, dummy.next
         while cur:
             if cur.random:
-                cur2.random = cache[cur.random.val]
-            cur = cur.next
-            cur2 = cur2.next
-        return head2.next
+                copy.random = cache[cur.random.val]
+            cur, copy = cur.next, copy.next
+        return dummy.next
