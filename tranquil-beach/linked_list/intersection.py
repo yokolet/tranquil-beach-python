e3dc12a191508bfa13ecdab412e8be27db1c2622
Yoko Harada <yokolet@gmail.com>
2019-09-16 14:32:37 -0700

    feat: adds more solution to intersection
    An existing solution, intersection of two linked lists, got another
    solution.

--- tranquil-beach/linked_list/intersection.py
+++ tranquil-beach/linked_list/intersection.py
@@ -1,19 +1,30 @@
 from .list_node import ListNode
 
 class Intersection:
-    def getIntersectionNode(self, headA, headB):
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        if not headA or not headB: return None
+        seen = set()
+        while headA:
+            seen.add(headA)
+            headA = headA.next
+        while headB:
+            if headB in seen:
+                return headB
+            headB = headB.next
+        return None
+
+    def getIntersectionNode2(self, headA, headB):
         """
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        if headA is None or headB is None:
-            return None
+        if not headA or not headB: return None
         
         curA, curB, lenA, lenB = headA, headB, 0, 0
-        while curA is not None:
+        while curA:
             lenA += 1
             curA = curA.next
-        while curB is not None:
+        while curB:
             lenB += 1
             curB = curB.next
         
