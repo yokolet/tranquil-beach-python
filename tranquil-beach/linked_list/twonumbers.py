c928e03dfa57b3021699ff8f4d4ac9ccc79d7e6e
Yoko Harada <yokolet@gmail.com>
2019-09-16 10:42:10 -0700

    chore: updates twonumbers
    An existing solution, add two numbers, was updated to have a better
    solution. Also, the comment was updated to a newer style.

--- tranquil-beach/linked_list/twonumbers.py
+++ tranquil-beach/linked_list/twonumbers.py
@@ -1,27 +1,18 @@
 from .list_node import ListNode
 
 class TwoNumbers:
-    def addTwoNumbers(self, l1, l2):
-        '''
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        '''
-        if not l1 or not l2:
-            return l1 or l2
-        head = l1
-        carry = 0
+     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1 or not l2: return l1 or l2
+        head, carry = l1, 0
         while l1 and l2:
-            carry, rem = divmod(l1.val + l2.val + carry, 10)
+            carry, rem = divmod(carry + l1.val + l2.val, 10)
             l1.val = rem
             cur, l1, l2 = l1, l1.next, l2.next
         cur.next = l1 or l2
-        while carry != 0:
-            if not cur.next:
-                cur.next = ListNode(carry)
-                break
-            else:
-                carry, rem = divmod(cur.next.val + carry, 10)
-                cur.next.val = rem
-                cur = cur.next
+        while carry and cur.next:
+            carry, rem = divmod(carry + cur.next.val, 10)
+            cur.next.val = rem
+            cur = cur.next
+        if carry:
+            cur.next = ListNode(carry)
         return head
