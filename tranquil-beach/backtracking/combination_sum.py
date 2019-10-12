299c4d6260ce234c1bf98b8dd0d14aeaeabfb60b
Yoko Harada <yokolet@gmail.com>
2019-10-11 20:32:56 -0700

    chore: updates combination sum
    An existing solution, combination sum, was updated to have the newer
    method signature.

--- tranquil-beach/backtracking/combination_sum.py
+++ tranquil-beach/backtracking/combination_sum.py
@@ -1,17 +1,12 @@
 class CombinationSum:
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        def dfs(arr, target, index, path, result):
+    def combinationSum(self, candidates: 'List[int]', target: int) -> 'List[List[int]]':
+        def dfs(arr, target, idx, path, result):
             if target == 0:
                 result.append(path)
                 return
-            while index < len(arr) and arr[index] <= target:
-                dfs(arr, target-arr[index], index, path+[arr[index]], result)
-                index += 1
+            while idx < len(arr) and arr[idx] <= target:
+                dfs(arr, target-arr[idx], idx, path+[arr[idx]], result)
+                idx += 1
         result = []
         dfs(sorted(candidates), target, 0, [], result)
         return result
