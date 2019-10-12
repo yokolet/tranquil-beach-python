2199b2abb8eb855b817b11b052bf43a4ad99f0fa
Yoko Harada <yokolet@gmail.com>
2019-10-11 22:52:34 -0700

    chore: updates merge intervals
    An existing solution, merge intervals, was updated in light of the
    change at platform.

--- tranquil-beach/sorting_searching/merge_intervals.py
+++ tranquil-beach/sorting_searching/merge_intervals.py
@@ -1,16 +1,11 @@
-from .interval import Interval
-
 class MergeIntervals:
-    def merge(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: List[Interval]
-        """
-        intervals.sort(key=lambda x: x.start)
-        result = []
-        for i in intervals:
-            if not result or result[-1].end < i.start:
-                result.append(i)
-            else:
-                result[-1].end = max(result[-1].end, i.end)
+    def merge(self, intervals: 'List[List[int]]') -> 'List[List[int]]':
+        if len(intervals) < 2: return intervals
+        intervals.sort(key=lambda x: x[0])
+        result = [intervals[0]]
+        for value in intervals[1:]:
+            if result[-1][1] < value[0]:
+                result.append(value)
+            elif result[-1][1] < value[1]:
+                result[-1][1] = value[1]
         return result
--- tranquil-beach/test/sorting_searching_test/test_merge_intervals.py
+++ tranquil-beach/test/sorting_searching_test/test_merge_intervals.py
@@ -1,24 +1,21 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 import unittest
-from sorting_searching.interval import Interval
 from sorting_searching.merge_intervals import MergeIntervals
 
 class TestMergeIntervals(unittest.TestCase):
     def setUp(self):
-        self.func = MergeIntervals()
+        self.func = MergeIntervals().merge
 
     def test_1(self):
         arr = [[1,3],[2,6],[8,10],[15,18]]
         expected = [[1,6],[8,10],[15,18]]
-        intervals = [Interval(s, e) for [s, e] in arr]
-        self.assertEqual([[i.start, i.end] for i in self.func.merge(intervals)], expected)
+        self.assertEqual(expected, self.func(arr))
 
     def test_2(self):
         arr = [[1,4],[4,5]]
         expected = [[1,5]]
-        intervals = [Interval(s, e) for [s, e] in arr]
-        self.assertEqual([[i.start, i.end] for i in self.func.merge(intervals)], expected)
+        self.assertEqual(expected, self.func(arr))
 
 if __name__ == '__main__':
     unittest.main()
